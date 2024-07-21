71d930bb049266615f383c5d9149551a46ea5d3f
Kaustubh Kolhe <kkaustubh903@gmail.com>
2024-07-21 23:54:36 +0530

    login 2res

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -13,10 +13,10 @@ const Login = () => {
 		e.preventDefault();
 		await login(username, password);
 	};
-
+// min-w-96 mx-auto
 	return (
 		<FormContainer>
-		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+		<div className='flex flex-col items-center justify-center '> 
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
 					Login
@@ -111,8 +111,8 @@ export default Login;
 
 
 const FormContainer = styled.div`
-//   height: 100vh;
-//   width: 100vw;
+  height: 100vh;
+  width: 100vw;
   display: flex;
   flex-direction: column;
   justify-content: center;
