439bca007233c7d4fb01cbfaf8b64d7e8f782411
Kaustubh Kolhe <kkaustubh903@gmail.com>
2024-07-28 14:31:51 +0530

    updated

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -25,7 +25,7 @@ const SignUp = () => {
 	// min-w-96 mx-auto
 	return (
 		<FormContainer >
-		<div className='flex flex-col items-center justify-center  bg-gray-900 res'>
+		<div className='flex flex-col items-center justify-center min-w-96 mx-auto bg-gray-900 res'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300 back'>
 					Sign Up <span className='text-purple-500'> ChatApp</span>
@@ -178,7 +178,6 @@ h1 {
 	  @media only screen and (max-device-width: 768px) {
 		
 		.res{
-			
 			height: 100vh;
             width: 100vw;
 		}
